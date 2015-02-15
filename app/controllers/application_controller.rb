748149a4766e4e051a21e7776d2e2acebed0af60
James Carson <jms.crsn@gmail.com>
2015-02-15 02:23:36 -0500

    forget the nav config. I want to see anything in views/foobar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :load_navigation_config
+
+  def load_navigation_config
+    @navigation_bar = Dir[Rails.root.to_s + "/app/views/foobar/*.html.haml"].collect {|x| x.split("/").last.split(".").first}
+  end
 end
