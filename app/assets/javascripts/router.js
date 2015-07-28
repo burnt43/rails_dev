4c1bf39d64af897c971243d7ded4a63372d3a0e6
James Carson <jcarson@monmouth.com>
2015-07-28 18:34:33 -0400

    set a root url

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,6 +1,12 @@
 // For more information see: http://emberjs.com/guides/routing/
 
+App.Router = Ember.Router.extend({
+  location: 'auto',
+  rootURL: '/'
+});
+
 App.Router.map(function() {
   this.route('posts');
   this.route('events');
 });
+
