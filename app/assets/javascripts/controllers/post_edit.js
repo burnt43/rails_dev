9c7e10c3120f952df77f5b7df101a4f2a5c0c72c
James Carson <jcarson@monmouth.com>
2015-08-03 09:33:49 -0400

    some changes

--- app/assets/javascripts/controllers/post_edit.js
+++ app/assets/javascripts/controllers/post_edit.js
@@ -1,2 +1,17 @@
 App.PostEditController = Ember.ObjectController.extend({
+
+  showUnsavedMessage: function () {
+    return this.get('model.hasDirtyAttributes') && !this.get('model.isSaving');
+  }.property('model.hasDirtyAttributes','model.isSaving'),
+
+  actions: {
+    saveChanges: function () {
+      this.get('model').save();
+      this.transitionToRoute('post');
+    },
+    cancel: function () {
+      this.get('model').rollback();
+      this.transitionToRoute('post');
+    }
+  }
 });
