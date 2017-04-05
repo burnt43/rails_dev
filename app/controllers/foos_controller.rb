d81f1e39bd650055192ea78400e5fa6800df2e15
James Carson <jcarson@monmouth.com>
2017-04-05 10:22:02 -0400

    .

--- app/controllers/foos_controller.rb
+++ app/controllers/foos_controller.rb
@@ -3,4 +3,40 @@ class FoosController < ApplicationController
     sleep 1
     @random_number = Random.rand(1000000)
   end
+
+  def new
+    @foo = Foo.new
+  end
+
+  def create
+    @foo = Foo.new
+    if @foo.update_attributes(foo_params)
+      flash[:notice] = {success: "#{Foo.model_name.human} has been created."}
+      redirect_to edit_foo_path(@foo)
+    else
+      flash[:notice] = {error: "#{Foo.model_name.human} could not be created."}
+      render :new
+    end
+  end
+
+  def edit
+    @foo = Foo.find(params[:id])
+  end
+
+  def update
+    @foo = Foo.find(params[:id])
+    if @foo.update_attributes(foo_params)
+      flash[:notice] = {success: "#{Foo.model_name.human} has been updated."}
+      redirect_to edit_foo_path(@foo)
+    else
+      flash[:notice] = {error: "#{Foo.model_name.human} could not be updated."}
+      render :edit
+    end
+  end
+
+  private
+
+  def foo_params
+    params.require(:foo).permit(:name, :title, :strength, :agility, :intelligence)
+  end
 end
--- app/models/foo.rb
+++ app/models/foo.rb
@@ -1,2 +1,7 @@
 class Foo < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :title
+  validates_presence_of :strength
+  validates_presence_of :agility
+  validates_presence_of :intelligence
 end
